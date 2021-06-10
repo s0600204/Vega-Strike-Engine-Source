ae60d89dd9976a125b3cef607d49b5e6a46f9b77
s0600204 <dev@s0600204>
2021-06-11 00:17:44 +0100

    Focus on openAL

--- find/script.sh
+++ find/script.sh
@@ -2,8 +2,15 @@
 
 set -e
 
-export PATH="/usr/local/opt/openal-soft/bin:$PATH"
+#export PATH="/usr/local/opt/openal-soft/bin:$PATH"
 
 mkdir find/temp && cd find/temp
 
+export OPENALDIR="/usr/local/opt/openal-soft/bin"
+cmake $@ ..
+
+export OPENALDIR="/usr/local/opt/openal-soft"
+cmake $@ ..
+
+export OPENALDIR="/usr/local/opt"
 cmake $@ ..
