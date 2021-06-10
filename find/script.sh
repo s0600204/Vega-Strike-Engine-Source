d873d7f5e014830026ad4dc66c643d1513a6517c
s0600204 <dev@s0600204>
2021-06-11 00:25:26 +0100

    Clear after each cmake run

--- find/script.sh
+++ find/script.sh
@@ -9,8 +9,18 @@ mkdir find/temp && cd find/temp
 export OPENALDIR="/usr/local/opt/openal-soft/bin"
 cmake $@ ..
 
+cd ..
+rm -r temp
+mkdir temp
+cd temp
+
 export OPENALDIR="/usr/local/opt/openal-soft"
 cmake $@ ..
+
+cd ..
+rm -r temp
+mkdir temp
+cd temp
 
 export OPENALDIR="/usr/local/opt"
 cmake $@ ..
