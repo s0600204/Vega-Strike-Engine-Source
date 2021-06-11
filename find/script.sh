be502ef0ed14ad6ac5fb7a8e950210d08ca81115
s0600204 <dev@s0600204>
2021-06-11 01:41:18 +0100

    Turn attention to OpenGL
    (which has no provision for alternate locations in cmake)

--- find/script.sh
+++ find/script.sh
@@ -2,25 +2,9 @@
 
 set -e
 
-#export PATH="/usr/local/opt/openal-soft/bin:$PATH"
 
 mkdir find/temp && cd find/temp
 
-export OPENALDIR="/usr/local/opt/openal-soft/bin"
-cmake $@ ..
-
-cd ..
-rm -r temp
-mkdir temp
-cd temp
-
+# This one is the winner!
 export OPENALDIR="/usr/local/opt/openal-soft"
-cmake $@ ..
-
-cd ..
-rm -r temp
-mkdir temp
-cd temp
-
-export OPENALDIR="/usr/local/opt"
 cmake $@ ..
